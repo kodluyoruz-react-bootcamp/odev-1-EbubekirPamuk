25ae3e17985c74a8579ce240983238fcb9f1ad44
EbubekirPamuk <58186104+EbubekirPamuk@users.noreply.github.com>
2020-12-26 07:57:57 +0300

    Add try and catch blocks

--- src/lib/service.js
+++ src/lib/service.js
@@ -3,10 +3,14 @@ import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com";
 
 async function getData(userID) {
+    try {
     const { data: users } = await axios.get(`${url}/users/${userID}`);
     const { data: posts } = await axios.get(`${url}/posts?userID=${userID}`);
 
-    console.log({ users: users, posts: posts });
+    console.log(...users, ...posts);
+    } catch(e){
+        console.log(e);
+    }
 };
 
 export default getData;
